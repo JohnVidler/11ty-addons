1c01202cebd99d0938a07c1f9fb01c5c64642cdf
Dr John Vidler <j.vidler@lancaster.ac.uk>
2024-06-14 15:17:02 +0100

    Logging writes to the stats object for debugging

--- plugins/nginx-pageviews.js
+++ plugins/nginx-pageviews.js
@@ -35,6 +35,8 @@ module.exports = function (eleventyConfig, pluginOptions) {
                         record.automata.push( agent[0] );
                 }
 
+                console.log( `\t${url} -> ${record.views}` );
+
                 stats[url] = record;
             }
         }
